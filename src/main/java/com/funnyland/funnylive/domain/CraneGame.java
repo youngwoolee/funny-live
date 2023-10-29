91fd86c637dbc96b30a1ca1cae5c4af067845626
joeylee <leeyw910205@gmail.com>
2023-10-30 00:57:12 +0900

    feat: 크레인 도메인 필드 추가

--- src/main/java/com/funnyland/funnylive/domain/CraneGame.java
+++ src/main/java/com/funnyland/funnylive/domain/CraneGame.java
@@ -2,6 +2,8 @@ package com.funnyland.funnylive.domain;
 
 import com.funnyland.funnylive.endpoint.cranegame.request.Position;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,14 +31,25 @@ public class CraneGame {
 
     private String imageUrl;
 
-    //TODO: 크레인용 상품?
+    //TODO: 크레인용 연결 상품
+
+    @Enumerated(EnumType.STRING)
+    private CraneGameState gameState;
 
     private int playFee;
+
+    private int timer;
 
     private Integer answerX; // 정답 x 좌표
     private Integer answerY; // 정답 y 좌표
 
     public boolean play(Position position) {
         return position.getX().equals(answerX) && position.getY().equals(answerY);
+    }
+
+    public enum CraneGameState {
+        GAME_NORMAL,    // 게임 중 (정상)
+        GAME_ERROR,     // 오류 상태
+        UNDER_INSPECTION // 점검 중
     }
 }
